3a082cf1a8de213f548f179753f5e8a7401e5277
SantosVicente <santos.vicente@aluno.ifsp.edu.br>
2023-10-08 17:31:01 -0300

    alterações no código

--- exercicio1/src/App.java
+++ exercicio1/src/App.java
@@ -3,16 +3,20 @@ public class App {
         Aluno aluno1 = new Aluno("João", 18, 'M', "1234");
         Aluno aluno2 = new Aluno("Maria", 19, 'F', "5678");
     
+        System.out.println();
+        
         System.out.println("Aluno 1:\n");
         System.out.println("Nome: " + aluno1.getNome());
         System.out.println("Idade: " + aluno1.getIdade());
         System.out.println("Sexo: " + aluno1.getSexo());
         System.out.println("Matricula: " + aluno1.getMatricula());
 
-        System.out.println("\nAluno 2:\n");
+        System.out.println("\n\nAluno 2:\n");
         System.out.println("Nome: " + aluno2.getNome());
         System.out.println("Idade: " + aluno2.getIdade());
         System.out.println("Sexo: " + aluno2.getSexo());
         System.out.println("Matricula: " + aluno2.getMatricula());
+        
+        System.out.println();
     }
 }
--- exercicio2/src/App.java
+++ exercicio2/src/App.java
@@ -3,6 +3,8 @@ public class App {
         Cachorro cachorro = new Cachorro("Rex", 10, 5, "Labrador");
         Gato gato = new Gato("Luna", 3, 2, "Branca");
 
+        System.out.println();
+        
         System.out.println("Cachcorro:\n");
         System.out.println("Nome: " + cachorro.getNome());
         System.out.println("Peso: " + cachorro.getPeso());
@@ -10,13 +12,15 @@ public class App {
         System.out.println("Raça: " + cachorro.getRaca());
         System.out.print(cachorro.getNome() + " disse: ");
         cachorro.emitirSom();
-
-        System.out.println("\nGato:\n");
+        
+        System.out.println("\n\nGato:\n");
         System.out.println("Nome: " + gato.getNome());
         System.out.println("Peso: " + gato.getPeso());
         System.out.println("Idade: " + gato.getIdade());
         System.out.println("Cor: " + gato.getCor());
         System.out.print(gato.getNome() + " disse: ");
         gato.emitirSom();
+
+        System.out.println();
     }
 }
